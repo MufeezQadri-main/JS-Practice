b382fc4f717b9d95aead6cea7a0526674c1d7fbf
Mufeez Qadri <73479962+MufeezQadri-main@users.noreply.github.com>
2026-01-21 09:50:14 +0000

    variable practice

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 1444553
+let accountEmail = "mufeez.qadri22@gmail.com"
+var accountPassword = "1234"
+accountCity = "Kashmir"
+let accountState;
+
+//acccountId = 2 //not allowed
+accountEmail = "mufeez@icloud.com"
+accountPassword = "12345678"
+accountCity = "Srinagar"
+console.table([accountEmail,accountPassword,accountCity,accountState]);
