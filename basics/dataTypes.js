b28baa8b87a1a5c84d1677ee58b376dd2dfa7a6e
Mufeez Qadri <73479962+MufeezQadri-main@users.noreply.github.com>
2026-01-21 11:35:26 +0000

    practice

---
+++ basics/dataTypes.js
@@ -0,0 +1,5 @@
+"use strict"; 
+console.log (3+3); console.log("Mufeez")
+    console.log(x)
+    const x = 10
+
---
+++ basics/functions.js
@@ -0,0 +1,13 @@
+function add(a,b) {
+    const c = a + b;   
+    console.log(c)
+}
+add(7,9);
+add(90, 150);
+
+function sayHello(){
+    return "Hello World"
+}
+let greeting = sayHello();
+console.log(greeting)
+sayHello();
